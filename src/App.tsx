e835c6c4ce64e587fce56b6f2f6916b91913c3e4
Ankush Garg <ankush2001garg@gmail.com>
2024-01-21 14:03:45 +0530

    Add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <div className="card">
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
-  </div>
+    <div className="card">
+      <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+    </div>
   )
 }
 
